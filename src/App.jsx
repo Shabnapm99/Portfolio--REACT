13c0bd1218f8d651803c8f01e21755811f162ea0
Shabna P M <pmshabnashareefa@gmail.com>
2026-02-06 21:40:40 +0300

    Created a basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <HeroSection/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
